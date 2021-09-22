fc17b1c2ac6a688c68d5ac735500c239b90f3dbd
Mark Blashki <markblashki1@gmail.com>
2021-09-22 13:46:16 +1000

    fix: added paging to dynamodb to fix large data requests failing add: added piecharts

--- frontend/src/pie.js
+++ frontend/src/pie.js
@@ -1,11 +1,10 @@
-import {Cell, Pie, PieChart, ResponsiveContainer} from 'recharts'
+import {Cell, Pie, PieChart} from 'recharts'
 
 const PieGraph = ({value, datakey, fill, max, size}) => {
   const data = [{name: datakey, value: value},{name: "nul", value: max-value}];
   const COLORS = [fill, '#D0D0D0'];
   return (
-    <ResponsiveContainer height={size} width={(size+10)*2}>
-      <PieChart>
+      <PieChart height={size} width={(size+10)*2}>
         <Pie
           data={data}
           cx={size}
@@ -22,7 +21,6 @@ const PieGraph = ({value, datakey, fill, max, size}) => {
         </Pie>
           <h2>Humidity: {value}</h2>
       </PieChart>
-    </ResponsiveContainer>
     );
 }
 
